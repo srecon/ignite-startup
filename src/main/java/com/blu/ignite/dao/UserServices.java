468a779576ba06b418ffe3d02c0221735d242825
shamim <shamimapache@gmail.com>
2016-02-16 14:56:15 +0300

    add

--- src/main/java/com/blu/ignite/dao/UserServices.java
+++ src/main/java/com/blu/ignite/dao/UserServices.java
@@ -1,7 +1,27 @@
 package com.blu.ignite.dao;
 
+import com.blu.ignite.dto.User;
+import com.blu.ignite.mapper.UserMapper;
+import org.springframework.cache.annotation.Cacheable;
+
 /**
  * Created by shamim on 16/02/16.
  */
 public class UserServices {
+    private UserMapper userMapper;
+
+
+    @Cacheable("returnHello")
+    public String sayhello(String str){
+        System.out.println("Client says:"+ str);
+
+        return "hello"+str;
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+    public User getUser(String id){
+        return userMapper.getUser(id);
+    }
 }
